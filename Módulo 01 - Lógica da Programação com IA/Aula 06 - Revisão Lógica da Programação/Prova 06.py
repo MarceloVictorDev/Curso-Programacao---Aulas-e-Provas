e5dcb87049e35b77067c93d09d142e5a0aade7dc
Marcelo Victor <mvictorcosta7@gmail.com>
2024-11-27 09:46:47 -0300

    Prova 06 - Sistema simples de Login
    Resolução da prova 06.
    Trata-se da criação de um sistema simples de login.

---
+++ Módulo 01 - Lógica da Programação com IA/Aula 06 - Revisão Lógica da Programação/Prova 06.py
@@ -0,0 +1,26 @@
+#[LPIA-A06]  Crie um programa em Python que simule um sistema de login. O programa deve permitir ao usuário três tentativas para acertar o nome de usuário e a senha corretos. Caso o usuário erre as credenciais, o programa deve fornecer uma mensagem informando quantas tentativas restam. Se o usuário acertar, uma mensagem de boas-vindas deve ser exibida, e o programa deve terminar imediatamente.
+#Se todas as três tentativas falharem, o programa deve usar um loop for para exibir uma mensagem de "Acesso bloqueado" repetida três vezes.
+
+nome_login = "infinity"
+senha_login = "dev1234"
+
+tentativas = 1
+def linhas():
+    print("-" * 30)
+
+while tentativas <= 3:
+    nome_usuario = input("Digite seu nome de usuário: ")
+    senha_usuario = input("Digite sua senha de usuário: ")
+    if nome_usuario == nome_login and senha_usuario == senha_login:
+        linhas()
+        print(f"Boas Vindas {nome_usuario}!")
+        linhas()
+        break
+    else:
+        print(f"Nome de usuário ou senha incorreta. Restam {3 - tentativas} tentativas.")
+        tentativas += 1
+if tentativas == 4:
+    for i in range(3):
+        linhas()
+        print("Acesso Bloqueado!")
+        linhas()
